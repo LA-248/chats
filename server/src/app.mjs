37096e6ec2ef89ef8a2ed3709ebf0661226a9fff
Luca <luca728@proton.me>
2024-05-20 17:24:58 +0200

    feat: Persist chat messages

--- server/src/app.mjs
+++ server/src/app.mjs
@@ -4,6 +4,7 @@ dotenv.config({ path: '../.env' });
 import express from 'express';
 import { createServer } from 'node:http';
 import { Server } from 'socket.io';
+import { db } from './services/database.mjs';
 
 const app = express();
 const server = createServer(app);
@@ -13,15 +14,42 @@ app.use(express.static('../../client/src/styles'));
 
 const io = new Server(server, {
   cors: { origin: '*' },
+  connectionStateRecovery: {},
 });
 
 io.on('connection', (socket) => {
   socket.on('chat message', (message) => {
+    try {
+      db.run('INSERT INTO messages (content) VALUES (?)', [message], (err) => {
+        if (err) {
+          console.error('Error inserting message into the database:', err.message);
+          return;
+        }
+      });
+    } catch (error) {
+      console.error('Unexpected error:', error.message);
+      return;
+    }
+
     io.emit('chat message', message);
   });
+
+  try {
+    db.each('SELECT content FROM messages', (err, row) => {
+      if (err) {
+        console.error('Error retrieving message content:', err.message);
+        return;
+      }
+
+      socket.emit('chat message', row.content);
+    });
+  } catch (error) {
+    console.error('Unexpected error:', error.message);
+    return;
+  }
 });
 
-const port = process.env.PORT;
+const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
---
+++ server/src/services/database.mjs
@@ -0,0 +1,15 @@
+import sqlite3 from 'sqlite3';
+
+const db = new sqlite3.Database('../db/database.db', (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Database connected.');
+  }
+});
+
+db.serialize(() => {
+  db.run('CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT)');
+});
+
+export { db };
