ca4441d816cbb443b5471c656545119f012ba384
Luca <luca728@proton.me>
2024-07-21 02:07:53 +0200

    refactor: Use state instead of DOM manipulation to display the chat delete button on hover

--- client/src/components/ChatList.jsx
+++ client/src/components/ChatList.jsx
@@ -1,31 +1,18 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { MessageContext } from './MessageContext';
 
 export default function ChatList({ userId, setSelectedChat, setUsername }) {
   const { activeChatId, setActiveChatId, chatList, setChatList } = useContext(MessageContext);
+  const [hoverChatId, setHoverChatId] = useState(null);
   const navigate = useNavigate();
   const storedChat = JSON.parse(localStorage.getItem('chat-list'));
 
   // Remove a conversation from the chat list
-  const removeObjectById = (id) => {
-    const updatedChatList = storedChat.filter(chat => chat.id !== id);
+  const removeChat = (id) => {
+    const updatedChatList = storedChat.filter((chat) => chat.id !== id);
     setChatList(updatedChatList);
     localStorage.setItem('chat-list', JSON.stringify(updatedChatList));
-  };
-
-  const displayChatDeleteButton = (event) => {
-    const chatDeleteButton = event.target.querySelector('.chat-delete-button');
-    if (chatDeleteButton) {
-      chatDeleteButton.style.display = 'block';
-    }
-  };
-
-  const removeChatDeleteButton = (event) => {
-    const chatDeleteButton = event.target.querySelector('.chat-delete-button');
-    if (chatDeleteButton) {
-      chatDeleteButton.style.display = 'none';
-    }
   };
 
   return (
@@ -38,8 +25,8 @@ export default function ChatList({ userId, setSelectedChat, setUsername }) {
               // Add the active class if the current chat's ID matches the activeChatId
               className={`chat-item ${chat.id === activeChatId ? 'active' : ''}`}
               key={chat.id}
-              onMouseEnter={displayChatDeleteButton}
-              onMouseLeave={removeChatDeleteButton}
+              onMouseEnter={() => setHoverChatId(chat.id)}
+              onMouseLeave={() => setHoverChatId(null)}
               onClick={() => {
                 setActiveChatId(chat.id);
                 setSelectedChat(chat.name);
@@ -52,18 +39,21 @@ export default function ChatList({ userId, setSelectedChat, setUsername }) {
                 <h4 className="chat-name">{chat.name}</h4>
                 <p className="chat-last-message">{storedChat[chat.id - 1].lastMessage}</p>
               </div>
+              <div className="container">
               <div className="chat-time">{storedChat[chat.id - 1].time}</div>
-              <button
-                onClick={(event) => {
-                  // Prevent the click event from also triggering the onClick event of the parent chat item
-                  event.stopPropagation();
-                  removeObjectById(chat.id);
-                }}
-                className="chat-delete-button"
-                style={{ display: 'none' }}
-              >
-                Delete
-              </button>
+                {hoverChatId === chat.id && (
+                  <button
+                    onClick={(event) => {
+                      event.stopPropagation();
+                      removeChat(chat.id);
+                      navigate('/');
+                    }}
+                    className="chat-delete-button"
+                  >
+                    Delete
+                  </button>
+                )}
+              </div>
             </div>
           </div>
         ))}
