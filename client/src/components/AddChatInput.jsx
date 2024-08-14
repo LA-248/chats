f952365128687ce35982460d4e079304a4f12d31
Luca <luca728@proton.me>
2024-08-14 17:13:46 +0200

    feat: Automatically clear error messages after timeout

--- client/src/components/AddChatInput.jsx
+++ client/src/components/AddChatInput.jsx
@@ -1,4 +1,18 @@
+import { useEffect } from 'react';
+
 export default function AddChatInput({ inputUsername, setInputUsername, handleAddChat, errorMessage, setErrorMessage}) {
+
+  // Clear error message after a certain amount of time
+  useEffect(() => {
+    if (errorMessage) {
+      const timer = setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [errorMessage, setErrorMessage]);
+  
   return (
     <div className="username-form-container">
       <form id="username-form" action="" onSubmit={handleAddChat}>
