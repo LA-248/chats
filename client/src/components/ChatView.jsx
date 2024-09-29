6a76c3dce3496ea95d5bd52d6f50aef3a2ee2b05
Luca <luca728@proton.me>
2024-09-29 20:01:43 +0200

    refactor: Create component for delete message modal

--- client/src/components/ChatView.jsx
+++ client/src/components/ChatView.jsx
@@ -2,19 +2,15 @@ import { useContext, useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSocket } from '../hooks/useSocket';
 import { MessageContext } from '../contexts/MessageContext';
-import { ChatContext } from '../contexts/ChatContext';
-import { deleteMessageById } from '../api/message-api';
-import { getChatListByUserId, updateChatList } from '../api/chat-api';
 import { useChatRoomEvents } from '../hooks/useChatRoomEvents';
 import ContactHeader from './ContactHeader';
 import MessageInput from './MessageInput';
-import Modal from './ModalTemplate';
 import handleModalOutsideClick from '../utils/ModalOutsideClick';
 import MessageList from './MessageList';
+import DeleteMessageModal from './DeleteMessageModal';
 
 function ChatView() {
   const { filteredMessages, setMessages } = useContext(MessageContext);
-  const { setChatList } = useContext(ChatContext);
   const { room } = useParams(); // Extract room from URL
   const socket = useSocket();
   const [messageId, setMessageId] = useState(null);
@@ -23,42 +19,6 @@ function ChatView() {
   const [hoveredIndex, setHoveredIndex] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
   const modalRef = useRef();
-
-  // Message deletion logic
-  const handleMessageDelete = async (messageId, messageIndex) => {
-    try {
-      const messageList = [...filteredMessages];
-      const isLastMessage = messageIndex === messageList.length - 1;
-      const newLastMessage = messageList.length - 2;
-
-      // If the message being deleted is the last one in the list (most recent message) -
-      // update the chat list with the new last message's details after deletion
-      if (isLastMessage && messageList.length > 1) {
-        await updateChatList(
-          messageList[newLastMessage].content,
-          messageList[newLastMessage].eventTime,
-          messageList[newLastMessage].eventTimeWithSeconds,
-          room
-        );
-        const storedChats = await getChatListByUserId();
-        setChatList(storedChats);
-        // If the message being deleted is the only message in the chat, clear the chat preview's content in the chat list
-      } else if (isLastMessage && messageList.length === 1) {
-        await updateChatList('', '', '', room);
-        const storedChats = await getChatListByUserId();
-        setChatList(storedChats);
-      }
-
-      // Delete the message from the database and update the message list state
-      await deleteMessageById(messageId);
-      messageList.splice(messageIndex, 1);
-
-      // Emit event to notify the server of message deletion and update the message list for everyone in the room
-      socket.emit('message-delete-event', room);
-    } catch (error) {
-      setErrorMessage(error.message);
-    }
-  };
 
   useChatRoomEvents(socket, room, setMessages, setErrorMessage);
 
@@ -85,40 +45,14 @@ function ChatView() {
         <MessageInput />
       </div>
 
-      <Modal
+      <DeleteMessageModal
+        messageId={messageId}
+        messageIndex={messageIndex}
         isModalOpen={isModalOpen}
         setIsModalOpen={setIsModalOpen}
         errorMessage={errorMessage}
         setErrorMessage={setErrorMessage}
-      >
-        <div className="modal-heading">Delete message</div>
-        <div className="modal-subtext" style={{ fontSize: '14px' }}>
-          Are you sure you want to delete this message? It will be deleted for
-          everyone.
-        </div>
-
-        <div className="modal-action-buttons-container">
-          <button
-            className="confirm-action-button"
-            style={{ backgroundColor: 'red' }}
-            onClick={() => {
-              handleMessageDelete(messageId, messageIndex);
-              setIsModalOpen(false);
-            }}
-          >
-            Delete
-          </button>
-
-          <button
-            className="close-modal-button"
-            onClick={() => {
-              setIsModalOpen(false);
-            }}
-          >
-            Close
-          </button>
-        </div>
-      </Modal>
+      />
     </div>
   );
 }
---
+++ client/src/components/DeleteMessageModal.jsx
@@ -0,0 +1,97 @@
+import { useParams } from 'react-router-dom';
+import { getChatListByUserId, updateChatList } from '../api/chat-api';
+import { deleteMessageById } from '../api/message-api';
+import { useSocket } from '../hooks/useSocket';
+import { useContext } from 'react';
+import { MessageContext } from '../contexts/MessageContext';
+import { ChatContext } from '../contexts/ChatContext';
+import Modal from './ModalTemplate';
+
+export default function DeleteMessageModal({
+  messageId,
+  messageIndex,
+  isModalOpen,
+  setIsModalOpen,
+  errorMessage,
+  setErrorMessage,
+}) {
+  const socket = useSocket();
+  const { room } = useParams();
+  const { filteredMessages } = useContext(MessageContext);
+  const { setChatList } = useContext(ChatContext);
+
+  // Message deletion logic
+  const handleMessageDelete = async (messageId, messageIndex) => {
+    try {
+      const messageList = [...filteredMessages];
+      const isLastMessage = messageIndex === messageList.length - 1;
+      const newLastMessage = messageList.length - 2;
+
+      // If the message being deleted is the last one in the list (most recent message) -
+      // update the chat list with the new last message's details after deletion
+      if (isLastMessage && messageList.length > 1) {
+        await updateChatList(
+          messageList[newLastMessage].content,
+          messageList[newLastMessage].eventTime,
+          messageList[newLastMessage].eventTimeWithSeconds,
+          room
+        );
+        const storedChats = await getChatListByUserId();
+        setChatList(storedChats);
+        // If the message being deleted is the only message in the chat, clear the chat preview's content in the chat list
+      } else if (isLastMessage && messageList.length === 1) {
+        await updateChatList('', '', '', room);
+        const storedChats = await getChatListByUserId();
+        setChatList(storedChats);
+      }
+
+      // Delete the message from the database and update the message list state
+      await deleteMessageById(messageId);
+      messageList.splice(messageIndex, 1);
+
+      // Emit event to notify the server of message deletion and update the message list for everyone in the room
+      socket.emit('message-delete-event', room);
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
+  };
+
+  return (
+    <>
+      <Modal
+        isModalOpen={isModalOpen}
+        setIsModalOpen={setIsModalOpen}
+        errorMessage={errorMessage}
+        setErrorMessage={setErrorMessage}
+      >
+        <div className="modal-heading">Delete message</div>
+        <div className="modal-subtext">
+          Are you sure you want to delete this message? It will be deleted for
+          everyone.
+        </div>
+
+        <div className="modal-action-buttons-container">
+          <button
+            className="confirm-action-button"
+            style={{ backgroundColor: 'red' }}
+            onClick={() => {
+              handleMessageDelete(messageId, messageIndex);
+              setIsModalOpen(false);
+            }}
+          >
+            Delete
+          </button>
+
+          <button
+            className="close-modal-button"
+            onClick={() => {
+              setIsModalOpen(false);
+            }}
+          >
+            Close
+          </button>
+        </div>
+      </Modal>
+    </>
+  );
+}
