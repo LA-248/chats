8bca1405a6f85cc82cb77c5da9c532396c7d2a3c
Luca <luca728@proton.me>
2025-01-15 23:09:14 +0100

    feat: Display private and group chats in user chat list

---
+++ server/src/models/chat-list-model.mjs
@@ -0,0 +1,76 @@
+import { pool } from '../../db/index.mjs';
+
+// Combine private and group chats to display them all in the user's chat list
+const Chat = {
+  retrieveAllChats: function (userId) {
+    return new Promise((resolve, reject) => {
+      pool.query(
+        `
+        SELECT
+          CONCAT('p_', pc.chat_id) AS chat_id,
+          CASE
+            WHEN pc.user1_id = $1 THEN pc.user2_id
+            ELSE pc.user1_id
+          END AS recipient_user_id,
+          u.username AS recipient_username,
+          u.profile_picture AS recipient_profile_picture,
+          pc.last_message_id,
+          m.content AS last_message_content,
+          m.event_time AS last_message_time,
+          pc.room,
+          CASE
+            WHEN pc.user1_id = $1 THEN pc.user1_read
+            WHEN pc.user2_id = $1 THEN pc.user2_read
+          END AS read,
+          'private_chat' AS chat_type,
+          pc.created_at,
+          pc.updated_at,
+          CASE
+            WHEN pc.user1_id = $1 THEN pc.user1_deleted
+            WHEN pc.user2_id = $1 THEN pc.user2_deleted
+          END AS user_deleted
+        FROM private_chats pc
+        JOIN users u ON u.user_id = CASE
+          WHEN pc.user1_id = $1 THEN pc.user2_id
+          ELSE pc.user1_id
+        END
+        LEFT JOIN messages m ON pc.last_message_id = m.message_id
+        WHERE (pc.user1_id = $1 OR pc.user2_id = $1)
+
+        UNION ALL
+
+        SELECT
+          CONCAT('g_', g.group_id) AS chat_id,
+          NULL as recipient_user_id,
+          g.name AS recipient_username,
+          g.group_picture AS recipient_profile_picture,
+          NULL AS last_message_id,
+          NULL AS last_message_content,
+          NULL AS last_message_time,
+          g.room,
+          NULL AS read,
+          'group' AS chat_type,
+          g.created_at,
+          NULL AS updated_at,
+          FALSE AS user_deleted
+        FROM groups g
+        JOIN group_members gm ON g.group_id = gm.group_id
+        WHERE gm.user_id = $1
+
+        ORDER BY last_message_time DESC NULLS LAST
+        `,
+        [userId],
+        (err, result) => {
+          if (err) {
+            return reject(
+              `Database error: ${err.message}`
+            );
+          }
+          return resolve(result.rows);
+        }
+      );
+    });
+  },
+};
+
+export { Chat };
--- server/src/models/group-model.mjs
+++ server/src/models/group-model.mjs
@@ -10,7 +10,7 @@ const Group = {
             owner_user_id INTEGER REFERENCES users(user_id) ON DELETE SET NULL,
             name TEXT,
             group_picture TEXT,
-            room UUID NOT NULL,
+            room TEXT NOT NULL,
             created_at TIMESTAMPTZ DEFAULT NOW()
           )
         `,
@@ -37,9 +37,58 @@ const Group = {
         [ownerUserId, name, room],
         (err, result) => {
           if (err) {
-            return reject(
-              `Database error in groups table: ${err.message}`
-            );
+            return reject(`Database error in groups table: ${err.message}`);
+          }
+          return resolve(result.rows[0]);
+        }
+      );
+    });
+  },
+
+  // READ OPERATIONS
+
+  retrieveGroupChats: function (userId) {
+    return new Promise((resolve, reject) => {
+      pool.query(
+        `
+        SELECT
+          g.group_id,
+          NULL as participant_id,
+          g.name AS participant_name,
+          g.group_picture AS participant_profile_picture,
+          NULL AS last_message_id,
+          NULL AS last_message_content,
+          NULL AS last_message_time,
+          g.room AS room,
+          'group' AS chat_type,
+          NULL AS is_read,
+          g.created_at,
+          NULL as updated_at,
+          NULL AS user_deleted
+        FROM groups g
+        JOIN group_members gm ON g.group_id = gm.group_id
+        WHERE gm.user_id = $1
+        ORDER BY last_message_time DESC NULLS LAST
+        `,
+        [userId],
+        (err, result) => {
+          if (err) {
+            return reject(`Database error in groups table: ${err.message}`);
+          }
+          return resolve(result.rows[0]);
+        }
+      );
+    });
+  },
+
+  retrieveGroupInfoByRoom: function (room) {
+    return new Promise((resolve, reject) => {
+      pool.query(
+        `SELECT group_id, name, group_picture FROM groups WHERE room = $1`,
+        [room],
+        (err, result) => {
+          if (err) {
+            return reject(`Database error in groups table: ${err.message}`);
           }
           return resolve(result.rows[0]);
         }
