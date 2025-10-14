66ebe8157a49ff2e6da08e399db9b6d005888a7e
Luca Antunes <104241771+LA-248@users.noreply.github.com>
2025-10-14 23:37:23 +0200

    fix: ensure private chats table is correctly created

--- server/db/index.ts
+++ server/db/index.ts
@@ -5,12 +5,12 @@ dotenv.config({
 
 import pg from 'pg';
 
-import { PrivateChat } from '../src/models/private-chat.model.ts';
 import { Group } from '../src/models/group.model.ts';
 import { GroupMember } from '../src/models/group-member.model.ts';
 import { Message } from '../src/models/message.model.ts';
 import { Session } from '../src/models/session.model.ts';
 import { User } from '../src/repositories/user.repository.ts';
+import { PrivateChat } from '../src/repositories/private-chat.repository.ts';
 const { Pool } = pg;
 
 // Initialise a connection pool
@@ -26,7 +26,10 @@ async function createTables(): Promise<void> {
   try {
     const userRepository = new User();
     await userRepository.createUsersTable();
-    await PrivateChat.createPrivateChatsTable();
+
+    const privateChatRepository = new PrivateChat();
+    await privateChatRepository.createPrivateChatsTable();
+
     await Group.createGroupsTable();
     await GroupMember.createGroupMemberTable();
     await Message.createMessagesTable();
