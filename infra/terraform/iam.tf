9210c1687104b90b88f7ba5f81893c555c4bcf29
Luca Antunes <104241771+LA-248@users.noreply.github.com>
2026-06-06 19:53:29 +0200

    feat: add IAM role and policy to push to ECR

---
+++ infra/terraform/iam.tf
@@ -0,0 +1,70 @@
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1"
+  ]
+}
+
+resource "aws_iam_role" "github_actions_ecr_push" {
+  name = "github-actions-ecr-push"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+            "token.actions.githubusercontent.com:sub" = "repo:LA-248/chats:*"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "github_actions_ecr_push" {
+  name        = "github-actions-ecr-push"
+  description = "Allow GitHub Actions to push images to ECR"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr-public:CompleteLayerUpload",
+          "ecr-public:UploadLayerPart",
+          "ecr-public:InitiateLayerUpload",
+          "ecr-public:BatchCheckLayerAvailability",
+          "ecr-public:PutImage",
+          "ecr-public:BatchGetImage"
+        ]
+        Resource = aws_ecrpublic_repository.this.arn
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr-public:GetAuthorizationToken",
+          "sts:GetServiceBearerToken"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_push" {
+  role       = aws_iam_role.github_actions_ecr_push.name
+  policy_arn = aws_iam_policy.github_actions_ecr_push.arn
+}
